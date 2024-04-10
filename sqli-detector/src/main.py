7ad65b18ad102b9288473508868328c7c339a1be
gunham01 <martinglovic77@gmail.com>
2024-04-10 09:22:54 +0700

    Thêm chức năng tìm kiếm

--- sqli-detector/src/main.py
+++ sqli-detector/src/main.py
@@ -1,18 +1,30 @@
+from pprint import pprint
 import pandas as pd
 import tkinter as tk
 from models_training import best_model
 from feature_selector import select_features
+from database import get_users_by_username
 
 light_gray = "#a8a4a3"
 arial_14 = ("Arial", 14)
 
 
-def find_user(_ = None):
+def on_detect(_=None):
     username = entry.get()
     if detect_sqli(username):
         result_label.config(text="Độc hại", fg="red")
     else:
         result_label.config(text="Lành tính", fg="green")
+
+
+def on_find_users(_=None):
+    username = entry.get()
+    users = get_users_by_username(username)
+    users_str = [str(user) for user in users]
+    if users:
+        result_label.config(text="\n".join(users_str), fg="green")
+    else:
+        result_label.config(text="Không tìm thấy", fg="red")
 
 
 def detect_sqli(content: str) -> bool:
@@ -28,7 +40,7 @@ def clear_input():
 
 root = tk.Tk()
 root.title("Phát hiện SQLi")
-root.geometry("400x200")
+root.geometry("400x300")
 
 tk.Label(root, text="Nhập query", font=arial_14).pack(pady=8, padx=4)
 
@@ -37,10 +49,12 @@ entry.pack(pady=8)
 
 buttons_frame = tk.Frame(root, border=0)
 buttons_frame.pack(pady=8)
-tk.Button(buttons_frame, text="Phát hiện", font=arial_14, command=find_user).pack(
+tk.Button(buttons_frame, text="Phát hiện", font=arial_14, command=on_detect).pack(
     side="left", padx=8
 )
-root.bind("<Return>", find_user)  # Nhấn Enter để phát hiện
+tk.Button(buttons_frame, text="Tìm kiếm", font=arial_14, command=on_find_users).pack(
+    side="left", padx=8
+)
 tk.Button(buttons_frame, text="Xoá", font=arial_14, command=clear_input).pack(
     side="left", padx=8
 )
