3a4c3ac6f7d5046928e5a00cf3beaf8184c9eba2
gunham01 <martinglovic77@gmail.com>
2024-04-12 17:25:38 +0700

    In lệnh SQL ra terminal

--- sqli-detector/src/database.py
+++ sqli-detector/src/database.py
@@ -1,11 +1,14 @@
 import mysql.connector
 
+
 def get_users_by_username(username):
     db_connector = mysql.connector.connect(
         host="localhost", user="root", password="", database="shopping_web_db"
     )
     db_cursor = db_connector.cursor()
-    db_cursor.execute(f"SELECT * FROM user WHERE username LIKE '%{username}%'")
+    query_str = f"SELECT * FROM user WHERE username LIKE '%{username}%'"
+    print(query_str)
+    db_cursor.execute(query_str)
     users = db_cursor.fetchall()
     db_connector.close()
     return users
